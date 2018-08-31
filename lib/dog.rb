c9cf2771711aeb3656fbe4482dc5e8cff9655d64
Thomas Pope <tpope0928@gmail.com>
2018-08-31 16:44:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,13 +10,13 @@ class Dog
   end
   
   def self.all
-    @@all.collect {|dog| dog.name}
-  end
-
-  def self.clear_all
     @@all.each do |dog|
       puts dog.name
     end
+  end
+
+  def self.clear_all
+    @@all.clear
   end
 
 
